4e8a3cf7d3ea4e2e3c1608a70b28b1256275b816
Federico Bruni <fede@inventati.org>
2020-05-12 09:27:24 +0200

    WIP: Add autocompletion for string tunings

--- frescobaldi_app/autocomplete/completiondata.py
+++ frescobaldi_app/autocomplete/completiondata.py
@@ -258,10 +258,11 @@ music_glyphs = listmodel.ListModel(ly.data.music_glyphs())
 
 midi_instruments = listmodel.ListModel(ly.words.midi_instruments)
 
+string_tunings = listmodel.ListModel(ly.words.string_tunings)
+
 language_names = listmodel.ListModel(sorted(ly.pitch.pitchInfo))
 
 def font_names():
     model = listmodel.ListModel(sorted(QFontDatabase().families()))
     model.setRoleFunction(Qt.FontRole, QFont)
     return model
-
